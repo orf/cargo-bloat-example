1a82326ca9c32b7acd15fac8ae021141b75e9785
Tom Forbes <tom@tomforb.es>
2020-04-26 22:11:51 +0100

    Flesh out the example project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use chrono::Duration;
+use chrono_humanize::{Accuracy, HumanTime, Tense};
+
 fn main() {
-    println!("Hello, world!");
+    let dt = Duration::days(45);
+    let ht = HumanTime::from(dt);
+    println!("{}", ht.to_text_en(Accuracy::Precise, Tense::Present));
 }
